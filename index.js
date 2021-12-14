4a216fb84574599b6091aa3c7409e9cd2c71ef8a
brendaspiv <89876003+brendaspiv@users.noreply.github.com>
2021-12-13 21:34:23 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const container = document.getElementById("container");
+const drama_btn = document.getElementById("drama_btn");
+// const comedy_btn = document.getElementById("comedy_btn");
+// const container = document.getElementById("container");
 
-const dramaImgArray = ["maid2.jpg", "static.jpg"];
+// const dramaImgArray = ["maid2.jpg", "static.jpg", "cat.jpg"];
 
-function changeDramaImg() {
-  const randomItemFromArray = Math.floor(Math.random() * dramaImgArray.length);
-  //document.body.style.backgroundImage = "url('maid2.jpg')";
-  container.style.backgroundImage = `url("maid2.jpg")`; // `url(${dramaImgArray[0].thumbnail})`;
-  // name.innerHTML = dramaImgArray[randomItemFromArray].name;]
-  console.log("changeDramaImg executed");
+// const imageSrcArray = ["maid2.jpg", "static.jpg", "cat.jpg"];
+
+// function changeDramaImg() {
+//   const randomItemFromArray = Math.floor(Math.random() * dramaImgArray.length);
+//   //document.body.style.backgroundImage = "url('maid2.jpg')";
+//   container.style.backgroundImage = `url("maid2.jpg")`; // `url(${dramaImgArray[0].thumbnail})`;
+//   // name.innerHTML = dramaImgArray[randomItemFromArray].name;]
+//   console.log("changeDramaImg executed");
+// }
+// // document.addEventListener("DOMContentLoaded", changeBackgroundImage());
+// drama_btn.addEventListener("click", () => {
+//   document.body.style.backgroundImage = ("click", imageSrcArray);
+// });
+// //  changeDramaImg);
+const imgPathArray = ["maid2.jpg", "cat.jpg", "static.jpg"];
+function changeImg() {
+  const randomImg = Math.floor(Math.random() * imgPathArray.length);
+
+  container.style.backgroundImage = `url(${randomImg})`;
 }
-// document.addEventListener("DOMContentLoaded", changeBackgroundImage());
-btn1.addEventListener("click", changeDramaImg);
+
+drama_btn.addEventListner("click", changeImg);
